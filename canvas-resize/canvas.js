8ee2887d1dbbff9b57a0185d2cdf7ebac8042999
PratikshaSh <pratiksha8520@gmail.com>
2021-04-20 02:15:47 +0530

    am=nimating canvas

--- canvas-resize/canvas.js
+++ canvas-resize/canvas.js
@@ -6,24 +6,24 @@ canvas.height = window.innerHeight;
 //contact
 //returning a drwing contact to variable c
 var c = canvas.getContext('2d');
-c.fillStyle = 'rgba(255,0,0,0.5)';
-c.fillRect(100, 100, 100, 100);
-c.fillStyle = 'rgba(0,255,0,0.5)';
-c.fillRect(400, 100, 100, 100);
-c.fillStyle = 'rgba(0,0,255,0.5)';
+// c.fillStyle = 'rgba(255,0,0,0.5)';
+// c.fillRect(100, 100, 100, 100);
+// c.fillStyle = 'rgba(0,255,0,0.5)';
+// c.fillRect(400, 100, 100, 100);
+// c.fillStyle = 'rgba(0,0,255,0.5)';
 
-c.fillRect(300, 300, 100, 100);
+// c.fillRect(300, 300, 100, 100);
 
 
-console.log(canvas);
+// console.log(canvas);
 
 //line
-c.beginPath();
-c.moveTo(50,300);
-c.lineTo(300, 100);
-c.lineTo(400, 300);
-c.strokeStyle = "#fa34a3"
-c.stroke();
+// c.beginPath();
+// c.moveTo(50,300);
+// c.lineTo(300, 100);
+// c.lineTo(400, 300);
+// c.strokeStyle = "#fa34a3"
+// c.stroke();
 
 //arc /circle
 // c.beginPath();
@@ -31,17 +31,78 @@ c.stroke();
 // c.strokeStyle = 'blue';
 // c.stroke();
 
-for(var i=0; i<1000; i++){
-    var x = Math.random() * window.innerWidth;
-    var y = Math.random() * window.innerHeight;
+// for(var i=0; i<1000; i++){
+//     var x = Math.random() * window.innerWidth;
+//     var y = Math.random() * window.innerHeight;
 
-    c.beginPath();
-    c.arc(x, y, 30, 0, Math.PI * 2, false);
-    c.strokeStyle =   "rgba(" + 
-                                Math.random() * 255 + "," + // Randomly generated brightness for color red 
-                                Math.random() * 255 + "," + // Randomly generated brightness for color green 
-                                Math.random() * 255 + "," + // Randomly generated brightness for color blue
-                                Math.random() + ")";             // Randomly generated transparency for alpha
-    c.stroke();
+//     c.beginPath();
+//     c.arc(x, y, 30, 0, Math.PI * 2, false);
+//     c.strokeStyle =   "rgba(" + 
+//                                 Math.random() * 255 + "," + // Randomly generated brightness for color red 
+//                                 Math.random() * 255 + "," + // Randomly generated brightness for color green 
+//                                 Math.random() * 255 + "," + // Randomly generated brightness for color blue
+//                                 Math.random() + ")";             // Randomly generated transparency for alpha
+//     c.stroke();
+
+// }
+
+// var x = Math.random() * innerWidth;
+// var y = Math.random() * innerHeight;
+// var dx = (Math.random() - 0.5) * 8;
+// var dy = (Math.random() - 0.5) * 8;
+// var radius = 30;
+
+//JS Object
+function Circle(x,y, dx, dy, radius){
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy =dy;
+    this.radius = radius;
+    
+    this.draw = function() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.strokeStyle = 'blue';
+        c.stroke();
+        c.fill();
+    }
+
+    this.update = function() {
+        if(this.x + this.radius > innerWidth || this.x - this.radius < 0){
+            this.dx = -this.dx;
+        }
+        if(this.y + this.radius > innerHeight || this.y - this.radius < 0){
+            this.dy = - this.dy;
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+        this.draw();
+    }
+}
+
+
+var circleArray = [];
+
+for(var i = 0; i<100; i++){
+    var radius = 30;
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
+    var dx = (Math.random() - 0.5) ;
+    var dy = (Math.random() - 0.5) ;
+    circleArray.push(new Circle(x,y,dx,dy,radius));
 
 }
+// console.log(circleArray);
+
+function animate(){
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
+
+    for(var i=0; i<circleArray.length; i++){
+       circleArray[i].update();
+    }
+    
+}
+
+animate();
